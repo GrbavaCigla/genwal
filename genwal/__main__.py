87180af8b2baee3ad281f2cc0ea879fd625f2b67
Aleksa Ognjanovic <alexa.ognjanovic@gmail.com>
2020-07-07 13:57:08 +0200

    Almost done

--- genwal/__main__.py
+++ genwal/__main__.py
@@ -3,28 +3,44 @@ import re
 import requests
 import os
 
-BACKGROUND = '{BACKGROUND}'
-FOREGROUND1 = '{FOREGROUND1}'
-FOREGROUND2 = '{FOREGROUND2}'
-FOREGROUND3 = '{FOREGROUND3}'
+from cairosvg import svg2png
 
 def color(color: str):
-    if not re.match(r'#[0-9a-fA-F]{6}', color):
+    if not re.match(r"#[0-9a-fA-F]{6}", color):
         raise argparse.ArgumentTypeError
     return color
 
+
 parser = argparse.ArgumentParser(
-    prog='genwal',
-    description='Little python script to generate Gentoo wallpapers',
-    add_help=True
+    prog="genwal",
+    description="Little python script to generate Gentoo wallpapers",
+    add_help=True,
 )
 
-parser.add_argument('--bg', '--background', default="#4D4D4D", type=color)
-parser.add_argument('--fg1', '--foreground1', default="#0D4F73", type=color)
-parser.add_argument('--fg2', '--foreground2', default="#FFFFFF", type=color)
-parser.add_argument('--fg3', '--foreground3', default="#007CBF", type=color)
+parser.add_argument("--bg", "--background", default="#4D4D4D", type=color)
+parser.add_argument("--fg1", "--foreground1", default="#0D4F73", type=color)
+parser.add_argument("--fg2", "--foreground2", default="#FFFFFF", type=color)
+parser.add_argument("--fg3", "--foreground3", default="#007CBF", type=color)
+parser.add_argument("--scale", default=1, type=int)
 
 args = parser.parse_args()
 
-with open("wallpaper.svg") as file:
-    pass
+replacements = {
+    "{BACKGROUND}": args.bg,
+    "{FOREGROUND1}": args.fg1,
+    "{FOREGROUND2}": args.fg2,
+    "{FOREGROUND3}": args.fg3,
+}
+
+response = requests.get("https://raw.githubusercontent.com/GrbavaCigla/genwal/master/wallpaper.svg")
+if response.status_code == 202:
+    svg_content = response.text
+
+    for k, v in replacements.items():
+        svg_content = re.sub(k, v, svg_content)
+
+    svg2png(
+        bytestring=svg_content, 
+        write_to="wallpaper.png",
+        scale=args.scale,
+    )
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='genwal-GrbavaCigla',
+    name='genwal',
     version='0.0.1',
     author='Aleksa Ognjanovic',
     author_email='alexa.ognjanovic@gmail.com',
